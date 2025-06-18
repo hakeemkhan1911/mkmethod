6b6837e10ab0d347a5a8cdb59644a136c2877538
hakeemkhan1911 <apkadukan1911@gmail.com>
2025-06-19 00:19:23 +0500

    Update settings.py

--- MK_method/settings.py
+++ MK_method/settings.py
@@ -33,8 +33,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',   
-    'whitenoise.middleware.WhiteNoiseMiddleware',  # add this right after SecurityMiddleware
-    'django.middleware.security.SecurityMiddleware',
 ]
 
 ROOT_URLCONF = 'MK_method.urls'
